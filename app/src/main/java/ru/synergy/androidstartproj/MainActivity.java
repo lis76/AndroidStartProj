318e61ec8460bea5e65f2423f27a325a5b885629
lis76 <rsavoskin@yandex.ru>
2022-12-20 20:50:34 +0300

    We have learned LinearLayout

--- app/src/main/java/ru/synergy/androidstartproj/MainActivity.java
+++ app/src/main/java/ru/synergy/androidstartproj/MainActivity.java
@@ -11,28 +11,31 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.second_layout);
+        setContentView(R.layout.activity_main);
 
-        TextView textView = (TextView) findViewById(R.id.header);
-
-        textView.setText("Hello from Java!");
-
-/*        ConstraintLayout constraintLayout = new ConstraintLayout(this);
-        TextView textView = new TextView(this);
-        textView.setText("Привет РОДНУЛЬКА!");
-        textView.setTextSize(26);
-
-        ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(
-                ConstraintLayout.LayoutParams.WRAP_CONTENT,
-                ConstraintLayout.LayoutParams.WRAP_CONTENT);
-
-        layoutParams.leftToLeft = ConstraintLayout.LayoutParams.PARENT_ID;
-        layoutParams.topToTop = ConstraintLayout.LayoutParams.PARENT_ID;
-        layoutParams.rightToRight = ConstraintLayout.LayoutParams.PARENT_ID;
-
-        textView.setLayoutParams(layoutParams);
-        constraintLayout.addView(textView);
-
-        setContentView(constraintLayout);*/
-    }
+///*
+//        TextView textView = (TextView) findViewById(R.id.header);
+//
+//        textView.setText("Hello from Java!");
+//
+//*/
+//        ConstraintLayout constraintLayout = new ConstraintLayout(this);
+//        TextView textView = new TextView(this);
+//        textView.setText("Привет РОДНУЛЬКА!");
+//        textView.setTextSize(26);
+//
+//        ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(
+//                ConstraintLayout.LayoutParams.WRAP_CONTENT,
+//                ConstraintLayout.LayoutParams.WRAP_CONTENT);
+//
+//        layoutParams.leftToLeft = ConstraintLayout.LayoutParams.PARENT_ID;
+//        layoutParams.topToTop = ConstraintLayout.LayoutParams.PARENT_ID;
+//        layoutParams.rightToRight = ConstraintLayout.LayoutParams.PARENT_ID;
+//
+//        textView.setLayoutParams(layoutParams);
+//        constraintLayout.addView(textView);
+//
+//        setContentView(constraintLayout);/*
+//
+   }
 }
