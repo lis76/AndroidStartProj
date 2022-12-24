421d68c9b6f470868b99f296c35edb38853f0d7c
lis76 <rsavoskin@yandex.ru>
2022-12-24 16:40:53 +0300

    Создание экрана настроек для игры

--- app/src/main/java/ru/synergy/androidstartproj/MainActivity.java
+++ app/src/main/java/ru/synergy/androidstartproj/MainActivity.java
@@ -12,36 +12,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.second_layout);
 
-        TextView textView = (TextView) findViewById(R.id.normal);
 
-        textView.setText("Text from Java code");
-        textView.setTextColor(Color.RED);
-
-///*
-//        TextView textView = (TextView) findViewById(R.id.header);
-//
-//        textView.setText("Hello from Java!");
-//
-//*/
-//        ConstraintLayout constraintLayout = new ConstraintLayout(this);
-//        TextView textView = new TextView(this);
-//        textView.setText("Привет РОДНУЛЬКА!");
-//        textView.setTextSize(26);
-//
-//        ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(
-//                ConstraintLayout.LayoutParams.WRAP_CONTENT,
-//                ConstraintLayout.LayoutParams.WRAP_CONTENT);
-//
-//        layoutParams.leftToLeft = ConstraintLayout.LayoutParams.PARENT_ID;
-//        layoutParams.topToTop = ConstraintLayout.LayoutParams.PARENT_ID;
-//        layoutParams.rightToRight = ConstraintLayout.LayoutParams.PARENT_ID;
-//
-//        textView.setLayoutParams(layoutParams);
-//        constraintLayout.addView(textView);
-//
-//        setContentView(constraintLayout);/*
-//
    }
 }
