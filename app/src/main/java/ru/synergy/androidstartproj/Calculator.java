55e8874312154458c661bd20d389b2bee8a1001f
lis76 <rsavoskin@andex.ru>
2023-01-19 09:18:53 +0300

    Context explanation

--- app/src/main/java/ru/synergy/androidstartproj/Calculator.java
+++ app/src/main/java/ru/synergy/androidstartproj/Calculator.java
@@ -1,12 +1,16 @@
 package ru.synergy.androidstartproj;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cursoradapter.widget.SimpleCursorAdapter;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListAdapter;
 import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -23,6 +27,15 @@ public class Calculator extends AppCompatActivity {
 
         final Button calculate = (Button) findViewById(R.id.button);
 
+        // Context trending
+//        TextView textView = new TextView(this);
+//        ListAdapter adapter = new SimpleCursorAdapter(getApplicationContext(),);
+//
+//                // Доступ из класса Activity -- наследник Context
+//        getSystemService(LAYOUT_INFLATER_SERVICE);
+//                // Shared prefs доступ с использованием контекста приложения
+//        SharedPreferences prefs = getApplicationContext().getSharedPreferences("PREFS", MODE_PRIVATE);
+        ////
         calculate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -122,6 +135,8 @@ public class Calculator extends AppCompatActivity {
         answer.setText("Готово же  " + solution);
         answer1.setText("Давай ещё )))");
 
+        Context contextApp = getApplicationContext();
+        Context context = getBaseContext();
 
     }
 }
