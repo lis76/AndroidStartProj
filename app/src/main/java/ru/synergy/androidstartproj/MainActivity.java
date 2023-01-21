9d3212f4bdd736345e4a0d71c0e465e17209a400
lis76 <rsavoskin@yandex.ru>
2023-01-21 20:10:00 +0300

    Added go to infActivity and simple open Activity

--- app/src/main/java/ru/synergy/androidstartproj/MainActivity.java
+++ app/src/main/java/ru/synergy/androidstartproj/MainActivity.java
@@ -7,13 +7,15 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final int REQ_C = 1;
     EditText et;
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +23,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.second_layout);
 
         et = (EditText) findViewById(R.id.et);
+        tv = (TextView) findViewById(R.id.tv);
+
+
+        Button bnt = (Button) findViewById(R.id.button);
+        Button bnt2 = (Button) findViewById(R.id.button2);
+        Button bnt3 = (Button) findViewById(R.id.button3);
+
+        bnt.setOnClickListener(this);
+        bnt2.setOnClickListener(this);
+        bnt3.setOnClickListener(this);
 
    }
 
+
+    @Override
     public void onClick(View v) {
         Intent i;
         switch (v.getId()){
